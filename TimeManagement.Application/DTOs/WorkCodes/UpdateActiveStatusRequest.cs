bed731b181b5d04d606babb6ff36e3f59910b650
AliNafees0800 <alihussain0800@gmail.com>
2025-10-17 15:13:13 +0500

    change

--- TimeManagement.Application/DTOs/WorkCodes/UpdateActiveStatusRequest.cs
+++ TimeManagement.Application/DTOs/WorkCodes/UpdateActiveStatusRequest.cs
@@ -9,11 +9,4 @@ public class UpdateActiveStatusRequest
     public bool IsActive { get; set; }
 }
 
-/// <summary>
-/// Request model for updating WorkCode Default Status
-/// </summary>
-public class UpdateDefaultStatusRequest
-{
-    public int WorkCodeId { get; set; }
-    public bool IsDefault { get; set; }
-}
+
---
+++ TimeManagement.Application/DTOs/WorkCodes/UpdateDefaultStatusRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace TimeManagement.Application.DTOs;
+
+/// <summary>
+/// Request model for updating WorkCode Default Status
+/// </summary>
+public class UpdateDefaultStatusRequest
+{
+    public int WorkCodeId { get; set; }
+    public bool IsDefault { get; set; }
+}
