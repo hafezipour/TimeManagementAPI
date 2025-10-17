e225fc4eb1518254ce79027fce056301b6dbb5e6
AliNafees0800 <alihussain0800@gmail.com>
2025-10-17 16:00:28 +0500

    change

--- TimeManagement.Application/DTOs/Shifts/SaveShiftRequest.cs
+++ TimeManagement.Application/DTOs/Shifts/SaveShiftRequest.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TimeManagement.Application.DTOs.Schedules;
 
 namespace TimeManagement.Application.DTOs.Shifts
 {
