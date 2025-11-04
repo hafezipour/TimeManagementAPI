d7f006c5709a140a6cbeade5ddce32771e667cc4
Ankur Arora <Ankur Arora@LAPTOP-RPMKLRAV>
2025-11-04 16:45:12 +0530

    Refactor SaveTradeBoardSettingsRequest class to include JsonPropertyName attributes for improved serialization

--- TimeManagement.Application/DTOs/TradeBoardSettings/SaveTradeBoardSettingsBatchRequest.cs
+++ TimeManagement.Application/DTOs/TradeBoardSettings/SaveTradeBoardSettingsBatchRequest.cs
@@ -16,19 +16,48 @@ public class SaveTradeBoardSettingsBatchRequest
 /// </summary>
 public class SaveTradeBoardSettingsRequest
 {
+    [JsonPropertyName("id")]
     public int Id { get; set; }
+
+    [JsonPropertyName("settingsName")]
     public string SettingsName { get; set; } = string.Empty;
+
+    [JsonPropertyName("isLimitTradesToMatchingLists")]
     public bool IsLimitTradesToMatchingLists { get; set; }
+
+    [JsonPropertyName("isOnlyAllowDirectTrades")]
     public bool IsOnlyAllowDirectTrades { get; set; }
+
+    [JsonPropertyName("isRequireApprovalBeforeSent")]
     public bool IsRequireApprovalBeforeSent { get; set; }
+
+    [JsonPropertyName("isRequireApprovalAfterAccepted")]
     public bool IsRequireApprovalAfterAccepted { get; set; }
+
+    [JsonPropertyName("isRequireSecondApproval")]
     public bool IsRequireSecondApproval { get; set; }
+
+    [JsonPropertyName("isApprovingUsersSeeOnlyTheirRequests")]
     public bool IsApprovingUsersSeeOnlyTheirRequests { get; set; }
+
+    [JsonPropertyName("isEnableShiftSwapFeature")]
     public bool IsEnableShiftSwapFeature { get; set; }
+
+    [JsonPropertyName("isColorCodeTradedShifts")]
     public bool IsColorCodeTradedShifts { get; set; }
+
+    [JsonPropertyName("tradedShiftColorCode")]
     public string? TradedShiftColorCode { get; set; }
+
+    [JsonPropertyName("isRequireManualLedgerApproval")]
     public bool IsRequireManualLedgerApproval { get; set; }
+
+    [JsonPropertyName("isUserSelectsApproval")]
     public bool IsUserSelectsApproval { get; set; }
+
+    [JsonPropertyName("isActive")]
     public bool IsActive { get; set; }
+
+    [JsonPropertyName("rules")]
     public List<object> Rules { get; set; } = new();
 }
