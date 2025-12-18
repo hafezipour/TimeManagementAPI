894689d1a0735cb6d150f2b508925cc3cf14febc
Your Name <alihussain0800@gmail.com>
2025-12-18 13:18:51 +0500

    changes

--- TimeManagement.Application/DTOs/ShiftAssignments/ShiftAssignmentDetailDto.cs
+++ TimeManagement.Application/DTOs/ShiftAssignments/ShiftAssignmentDetailDto.cs
@@ -42,8 +42,10 @@ public class ShiftAssignmentDetailDto
     // Time off information
     public TimeOffStatus? TimeOffStatus { get; set; }
     public List<TimeOffRequestsForUsers>? TimeOffRequests { get; set; }
-    
+    public int? IsChild { get; set; }//This is to indicate its trade record or not
+
     // Assignment date range
+
     public DateTime? FromDate { get; set; }
     public DateTime? ToDate { get; set; }
 }
