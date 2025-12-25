4369f8d913cb98a2163b42f102024bf4a29915d1
Your Name <alihussain0800@gmail.com>
2025-12-25 13:27:24 +0500

    change

--- TimeManagement.Application/Processors/ShiftProcessor.cs
+++ TimeManagement.Application/Processors/ShiftProcessor.cs
@@ -664,7 +664,7 @@ public class ShiftProcessor : BaseProcessor
 
             allSchedules = JsonConvert.DeserializeObject<List<DTOs.Schedules.ScheduleResponse>>(schedulesJson);
         }
-        allSchedules = allSchedules.Where(c => c.Id == 170).ToList();
+        //allSchedules = allSchedules.Where(c => c.Id == 170).ToList();
 
         foreach (var shift in schedulingShiftsAll)
         {
